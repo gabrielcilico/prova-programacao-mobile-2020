62d50509fca7b4b556420efa81644c18e9f99396
gabrielcilico <gabrielcilico@gmail.com>
2020-09-25 00:32:00 -0300

    Questao 7 - Criando tela de Splash

---
+++ app/src/main/java/com/example/provasplash/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.provasplash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    private static int TEMPO_DE_EXIBICAO = 4000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(executaSplash(), TEMPO_DE_EXIBICAO);
+    }
+
+    public Runnable executaSplash() {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        };
+    }
+}
